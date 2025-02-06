afdd5ee72bdb71396daf4c0d0df1927d669150f4
--global <royalewhat00@gmail.com>
2025-02-06 18:56:50 +0100

    1pt 3act

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+class Settings(BaseSettings):
+    DB_HOST = str
+    DB_PORT = int
+    DB_NAME = str
+    DB_USER = str
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
+    )
+
+settings = Settings()
+
+def get_db_url():
+    return f'postgres+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
