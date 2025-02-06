9f187e4ba4c68f11106fe070148b94050960a52f
balddr_odinson <boliklevik@gmail.com>
2025-02-07 00:26:32 +0100

    3act

--- app/config.py
+++ app/config.py
@@ -2,10 +2,11 @@ import os
 from pydantic_settings import SettingsConfigDict, BaseSettings
 
 class Settings(BaseSettings):
-    DB_HOST = str
-    DB_PORT = int
-    DB_NAME = str
-    DB_USER = str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
     )
@@ -13,4 +14,4 @@ class Settings(BaseSettings):
 settings = Settings()
 
 def get_db_url():
-    return f'postgres+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
+    return f'postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
